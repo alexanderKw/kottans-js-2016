b38ea145074b8a0d9a3e56433029e9cc4b2d0b37
alexanderKw <alkrivulya@gmail.com>
2016-06-07 15:52:05 +0300

    deepAssign

--- week03/deepAssign.js
+++ week03/deepAssign.js
@@ -33,47 +33,4 @@
     configurable: true
   })
 
-  var obj1 = {
-    a: 9,
-    b: 'a'
-  }
-  var obj2 = Object.create(null)
-  var obj2 = {
-    'null': 'null',
-    get() {
-      console.log('Hello!')
-    }
-  }
-  var obj3 = {
-    a: 23,
-    b: 'tr',
-    c: 10,
-    d: 89
-  }
-  var obj4 = {
-    a: {j: 'jj', m: {g: 4, r: 'four'}},
-    foo() {
-      alert('Hello')
-    },
-    b: 'bb',
-    c: null,
-    d: false
-  }
-  var obj5 = {
-      a: [5, 4, 6],
-      b: 2,
-      c: new Set([1, 2, 3]),
-      d: new Map([[1, 2], ['some str', 5]]),
-      e: /smth/ig,
-      f: {
-          a: 3,
-          b: 4,
-          c: {
-              d: new Date()
-          }
-      }
-  }
-
-  console.log(Object.deepAssign(obj1, obj2, obj3, obj4, obj5))
-
 })()
