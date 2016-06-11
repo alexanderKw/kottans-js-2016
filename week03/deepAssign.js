037fd005603b2eba9b92cfc0b276fe1ebb11fbff
alexanderKw <alkrivulya@gmail.com>
2016-06-11 04:03:21 +0300

    deepAssign

--- week03/deepAssign.js
+++ week03/deepAssign.js
@@ -1,36 +1,50 @@
-(function() {
-  'use strict'
+(function()
+{
+    'use strict'
 
-  if (typeof Object.deepAssign == 'function') return
+    if(typeof Object.assign == 'assign') return
 
-  var isEnumerable = Object.prototype.propertyIsEnumerable
+    var isEnumerable = {}.propertyIsEnumerable
 
-  Object.defineProperty(Object, 'deepAssign',
-  {
-    value: function deepAssign(target, sources)
+    Object.defineProperty(Object, 'deepAssign',
     {
-      if (target == null || !(target instanceof Object) || !(target.__proto__.constructor == Object))
-        throw new TypeError('Cannot convert undefined or null to object')
-
-      var to = Object(target)
-      for (var index = 1; index < arguments.length;)
-      {
-        var from = arguments[index++]
-        if (from !== Object(from)) continue
-
-        Reflect.ownKeys(from).forEach(function(key)
+        value: function deepAssign(target, sources)
         {
-          if (isEnumerable.call(from, key)){
-            to[key] = from[key]
-          }
+            if(target == null)
+                throw new TypeError('Cannot convert undefined or null to object')
 
-        })
-      }
+            var to = Object(target)
 
-      return to
-    },
-    writable: true,
-    configurable: true
-  })
+            for(var index = 1; index < arguments.length;)
+            {
+                var from = arguments[index++]
+                if(from !== Object(from)) continue
+
+                Reflect.ownKeys(from).forEach(function(key)
+                {
+
+                    if(from[key] !== null && from[key] instanceof Object && from[key].__proto__.constructor == Object)
+                    {
+                        if(from[key] instanceof Date || from[key] instanceof Set || from[key] instanceof Map || from[key] instanceof RegExp)
+                        {
+                            to[key] = new from[key].constructor(from[key])
+                        } else {
+                            deepAssign(to, from[key])
+                        }
+                    }
+
+                    if(isEnumerable.call(from, key))
+                    {
+                        to[key] = from[key]
+                    }
+
+                })
+            }
+
+            return to
+        },
+        writable: true,
+        configurable: true
+    })
 
 })()
